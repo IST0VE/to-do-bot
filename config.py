24c467663a4ca0121e17f779c7dec4e9e9576f00
Nikita <74756686+IST0VE@users.noreply.github.com>
2023-11-02 15:13:40 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-TELEGRAM_TOKEN = "5930966102:AAEd9_0J4hL8yahkNYizF5fRz39euUBeLBg"
+TELEGRAM_TOKEN = ""
 
-FILE_STORAGE_PATH = "/Users/nikitakorovko/Desktop/Lab_1/Bots_tg/to-do"
+FILE_STORAGE_PATH = ""
 
-host = "mysql.j98515082.myjino.ru"
-user = "j98515082"
-password = "j98515082"
-database = "j98515082_tasks"
+host = ""
+user = ""
+password = ""
+database = ""
